3bcbc66ee608fed17a67bf4c30dd7ecc65d1d867
Aymane <aymanebk@gmail.com>
2022-10-22 19:23:09 +0100

    Add buttons to change number of pixels

--- script.js
+++ script.js
@@ -2,24 +2,64 @@ const pixelsContainer = document.querySelector('.pixels-container');
 
 const pixel = document.createElement("div");
 pixel.className ="pixel";
-pixel.style.cssText = 'width: 28.25px; height:28.25px; auto; border: thin solid grey;';
+pixel.style.cssText = 'width: 26px; height:26px; auto; border: 1px solid grey;';
 
-// Append "pixels" to their div
 
-for(i =0 ; i<16; i++){
-    for(j = 0; j<16; j++){
-        pixelsContainer.append(pixel.cloneNode(true));
+//fill the canvas with "pixels"
+
+function filler(numberOfPixels){
+    for(i =0 ; i<numberOfPixels; i++){
+        for(j = 0; j<numberOfPixels; j++){
+            pixelsContainer.append(pixel.cloneNode(true));
+        };
+        
     };
-    
-};
+}
 
-// select every "pixel" and attach an event listener to change color on hover
-
-const allOfEm = document.querySelectorAll('.pixel');
-
-allOfEm.forEach((square) => {
-    square.addEventListener('mouseover', ()=> {
-        square.style.backgroundColor = "Black";
+//attach event listener to every "pixel" to change background on hover
+function attacher(){
+    const allOfEm = document.querySelectorAll('.pixel');
+    allOfEm.forEach((square) => {
+        square.addEventListener('mouseover', ()=> {
+            square.style.backgroundColor = "Black";
+        });
     });
+}
+
+function defaultGrid(){
+    filler(16);
+    attacher();
+}
+
+defaultGrid();
+
+const sixteen = document.querySelector('.sixteen');
+const thirtyTwo = document.querySelector('.thirtytwo');
+const sixtyFour = document.querySelector('.sixtyfour');
+const hundredTwentyEight = document.querySelector('.h-twentyeight');
+
+thirtyTwo.addEventListener('click', () =>{
+    pixelsContainer.textContent = '';
+    pixel.style.width = `${(448/32) - 2}px`;
+    pixel.style.height = `${(448/32) - 2}px`;
+    filler(32);
+    attacher();
+
 });
 
+sixteen.addEventListener('click', () =>{
+    pixelsContainer.textContent = '';
+    pixel.style.width = `${(448/16) - 2}px`;
+    pixel.style.height = `${(448/16) - 2}px`;
+    filler(16);
+    attacher();
+
+});
+sixtyFour.addEventListener('click', () =>{
+    pixelsContainer.textContent = '';
+    pixel.style.width = `${(448/64) - 2}px`;
+    pixel.style.height = `${(448/64) - 2}px`;
+    filler(64);
+    attacher();
+
+});
